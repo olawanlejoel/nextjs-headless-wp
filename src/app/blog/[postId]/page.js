88c4f5c877048813775116e54867300bc8438d11
olawanlejoel <info4tobest2@gmail.com>
2023-12-12 16:07:40 +0100

    first commit

---
+++ src/app/blog/[postId]/page.js
@@ -0,0 +1,26 @@
+async function getSinglePost(postId) {
+	const response = await fetch(
+		`https://duketest.kinsta.cloud/wp-json/wp/v2/posts/${postId}`
+	);
+	const post = await response.json();
+	return post;
+}
+
+const page = async ({ params }) => {
+	const post = await getSinglePost(params.postId);
+
+	if (!post) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<div className="single-blog-page">
+			<h2>{post.title.rendered}</h2>
+			<div className="blog-post">
+				<p dangerouslySetInnerHTML={{ __html: post.content.rendered }}></p>
+			</div>
+		</div>
+	);
+};
+
+export default page;
