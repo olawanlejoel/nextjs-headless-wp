aa91a69f3bb1e93bb8c9f695a0fabfb9f1ecfb51
olawanlejoel <info4tobest2@gmail.com>
2024-02-09 12:30:51 +0100

    starter files

--- src/app/blog/[postId]/page.js
+++ src/app/blog/[postId]/page.js
@@ -1,36 +1,14 @@
-export const dynamicParams = true;
-
-export async function generateStaticParams() {
-	const response = await fetch(
-		`https://duketest.kinsta.cloud/wp-json/wp/v2/posts`
-	);
-	const posts = await response.json();
-
-	return posts.map((post) => ({
-		postId: post.id.toString(),
-	}));
-}
-
-async function getSinglePost(postId) {
-	const response = await fetch(
-		`https://duketest.kinsta.cloud/wp-json/wp/v2/posts/${postId}`
-	);
-	const post = await response.json();
-	return post;
-}
-
-const page = async ({ params }) => {
-	const post = await getSinglePost(params.postId);
-
-	if (!post) {
-		return <div>Loading...</div>;
-	}
-
+const page = async () => {
 	return (
 		<div className="single-blog-page">
-			<h2>{post.title.rendered}</h2>
+			<h2>Welcome to Lorem</h2>
 			<div className="blog-post">
-				<p dangerouslySetInnerHTML={{ __html: post.content.rendered }}></p>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur
+					ea dolore blanditiis. Eius natus incidunt doloribus exercitationem
+					dolorum, vero pariatur modi sapiente dignissimos iste quam facilis
+					aperiam. Ut, veritatis esse?
+				</p>
 			</div>
 		</div>
 	);
--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,31 +1,25 @@
 import Link from 'next/link';
 
-async function getPosts() {
-	const response = await fetch(
-		'https://duketest.kinsta.cloud/wp-json/wp/v2/posts'
-	);
-	const posts = await response.json();
-	return posts;
-}
-
 const BlogPage = async () => {
-	const posts = await getPosts();
-
 	return (
 		<div className="blog-page">
 			<h2>All Blog Posts</h2>
 			<p>All blog posts are fetched from WordPress via the WP REST API.</p>
 			<div className="posts">
-				{posts.map((post) => {
-					return (
-						<Link href={`/blog/${post.id}`} className="post" key={post.id}>
-							<h3>{post.title.rendered}</h3>
-							<p
-								dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
-							></p>
-						</Link>
-					);
-				})}
+				<Link href="/blog/1" className="post">
+					<h3>Hello World</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae
+						molestiae ducimus!
+					</p>
+				</Link>
+				<Link href="/blog/2" className="post">
+					<h3>Hello World</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae
+						molestiae ducimus!
+					</p>
+				</Link>
 			</div>
 		</div>
 	);
